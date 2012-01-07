d44c943475f1762ac8fdbd34644f163f0d4dd983
Jeff Standen <jeff@webgroupmedia.com>
2012-01-07 15:10:47 -0800

    Added Virtual Attendant behavior + macro variable support

--- api/App.php
+++ api/App.php
@@ -114,7 +114,6 @@ class WgmTwilio_EventActionSendSms extends Extension_DevblocksEventAction {
 	function render(Extension_DevblocksEvent $event, Model_TriggerEvent $trigger, $params=array(), $seq=null) {
 		$tpl = DevblocksPlatform::getTemplateService();
 		$tpl->assign('params', $params);
-		$tpl->assign('token_labels', $event->getLabels());
 		
 		if(!is_null($seq))
 			$tpl->assign('namePrefix', 'action'.$seq);
